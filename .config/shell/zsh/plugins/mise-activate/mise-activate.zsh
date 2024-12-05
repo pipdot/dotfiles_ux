76ced2188d1d08dd3bc7a3114a2140c7535d89da
piptouque <pierre.thiel@touque.fr>
2024-12-05 12:05:19 +0100

    Dotfiles managed by DFM! https://github.com/chasinglogic/dfm

--- .config/shell/zsh/plugins/mise-activate/mise-activate.zsh
+++ .config/shell/zsh/plugins/mise-activate/mise-activate.zsh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 if [ -x "$(command -v ~/.local/bin/mise)" ]; then
   eval "$(~/.local/bin/mise activate zsh)"
-elif[ -x "$(command -v mise)" ]; then
+elif [ -x "$(command -v mise)" ]; then
   eval "$(mise activate zsh)"
 fi
 
